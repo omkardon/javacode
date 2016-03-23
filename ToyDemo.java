9acc5e3effc92d787f005d6235a3026c2157b3e4
omkardon <omkardon@users.noreply.github.com>
2016-03-23 08:25:15 +0530

    Create ToyDemo.java

---
+++ ToyDemo.java
@@ -0,0 +1,38 @@
+package com;
+
+public class ToyDemo {
+
+	public static void main(String[] args) {
+		Toy t1=new Toy("elephant","kids",80,10);
+		Toy t2=new Toy("bike","kids",120,15);
+		Toy t3=new Toy("kitchen set","kids",100,5);
+		Toy t4=new Toy("Gun","kids",200,30);
+		
+		
+
+	}
+	public Static String getLeastPriceToy(Toy t1, Toy t2, Toy t3, Toy t4, String param_category) {
+		double [] price= new double [4];
+		if(param_category=="") {
+			return "no category found";
+		}
+		
+		if(t1.getCategory().equals(param_category)) {
+			price[0]=t1.getPrice();
+		}
+		if(t2.getCategory().equals(param_category)) {
+			price[1]=t1.getPrice();
+		}
+		if(t3.getCategory().equals(param_category)) {
+			price[2]=t1.getPrice();
+		}
+		if(t4.getCategory().equals(param_category)) {
+			price[3]=t1.getPrice();
+		}
+		//Calculate highest among prices
+		//return t.name as output
+		//like if price-price*discount*(1/100) is least
+		//return its name as output
+	}
+
+}
