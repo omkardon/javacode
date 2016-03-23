209739ada1d6f34c2ee25aa2a1d1deb02fb31130
omkardon <omkardon@users.noreply.github.com>
2016-03-23 09:22:48 +0530

    Update CreditCardCompany.java

--- CreditCardCompany.java
+++ CreditCardCompany.java
@@ -7,16 +7,15 @@ public class CreditCardCompany {
 		
 		double charge=customer.getCreditCardCharges();
 		
-		if(charge>0)
-			if(charge<500) {
+			if(charge>0) {
 				payBackAmount+=0.0025*charge;
 				charge-=500;
 			}
-			if(charge<1500)  {
+			if(charge>0) {
 				payBackAmount+=0.005*charge;
 				charge-=1000;
 			}
-			if(charge<2500) {
+			if(charge>0) {
 				payBackAmount+=0.0075*charge;
 				charge-=1000;
 			}
