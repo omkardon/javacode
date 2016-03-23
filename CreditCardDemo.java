43f08e9defbb8fe3fa71a57780f0206fb1f5935c
omkardon <omkardon@users.noreply.github.com>
2016-03-23 09:42:25 +0530

    Update CreditCardDemo.java

--- CreditCardDemo.java
+++ CreditCardDemo.java
@@ -9,14 +9,14 @@ public class CreditCardDemo {
 		
 		Customer [] cust = new Customer [5];
 		
-		cust[0]=new Customer(10, 20, company.getPaybackAmount(cust[0]));
-		cust[1]=new Customer(11, 21, company.getPaybackAmount(cust[1]));
-		cust[2]=new Customer(12, 22, company.getPaybackAmount(cust[2]));
-		cust[3]=new Customer(13, 23, company.getPaybackAmount(cust[3]));
-		cust[4]=new Customer(14, 24, company.getPaybackAmount(cust[4]));
+		cust[0]=new Customer(10, 20, 2800);
+		cust[1]=new Customer(11, 21, 3500);
+		cust[2]=new Customer(12, 22, 4500);
+		cust[3]=new Customer(13, 23, 1200);
+		cust[4]=new Customer(14, 24, 3400);
 		
 		for(Customer customer:cust) {
-			System.out.println(customer.getCreditCardCharges());
+			System.out.println(company.getPaybackAmount(customer));
 		}
 
 	}
