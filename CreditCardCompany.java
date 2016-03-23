702871003add203d5c1e8eae18e1b27a3d48309b
omkardon <omkardon@users.noreply.github.com>
2016-03-23 09:06:36 +0530

    Create CreditCardCompany.java

---
+++ CreditCardCompany.java
@@ -0,0 +1,13 @@
+package com;
+
+public class CreditCardCompany {
+	public double getPaybackAmount(Customer customer) {
+		
+		double payBackAmount=0.0;
+		
+		//Some Calculation
+		//Use given Logic
+		
+		return payBackAmount;
+	}
+}
