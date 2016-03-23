3591317fc0b4ee669efcca641a7db2642cb05068
omkardon <omkardon@users.noreply.github.com>
2016-03-23 19:56:16 +0530

    Create ItemDemo.java

---
+++ ItemDemo.java
@@ -0,0 +1,28 @@
+package com;
+
+public class ItemDemo {
+	public static void main(String [] args) {
+		Item [] item=new Item[5];
+		item[0]=new Item(1,"Floppy",100,5);
+		item[1]=new Item(1,"Floppy",100,5);
+		item[2]=new Item(1,"Floppy",100,5);
+		item[3]=new Item(1,"Floppy",100,5);
+		item[4]=new Item(1,"Floppy",100,5);
+		
+		System.out.println(getLeastPriceItem(item));
+	}
+	
+	public static Item getLeastPriceItem(Item[] items) {
+		double min=items[0].getItemPrice()-(1/100)*items[0].getItemPrice()*items[0].getItemDiscount();
+		Item ret_item=items[0];
+		
+		for(Item element:items) {
+			double price=element.getItemPrice()-(1/100)*items[0].getItemPrice()*items[0].getItemDiscount();
+			if(price<min) {
+				min=price;
+				ret_item=element;
+			}
+		}
+		return ret_item;
+	}
+}
