8201c1ed2f7da7c6d755bdfd300487b249ed7e82
omkardon <omkardon@users.noreply.github.com>
2016-03-23 08:24:29 +0530

    Create Toy.java

---
+++ Toy.java
@@ -0,0 +1,39 @@
+package com;
+
+public class Toy {
+	private String name;
+	private String category;
+	private double price;
+	private double discount;
+	public Toy(String name, String category, double price, double discount) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.discount = discount;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+}
